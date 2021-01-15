76c447a6c8064ca1aee71fe7b62fddc018df5471
Elijah Littell <elittell214@gmail.com>
2021-01-15 02:30:15 -0500

    Added a Footer Component

---
+++ lookatmefile/src/components/Footer.js
@@ -0,0 +1,11 @@
+import './../css/Footer.css';
+
+function Footer() {
+    return (
+        <div className="footer">
+            @CyberNotesDev {new Date().getFullYear()}
+        </div>
+    );
+}
+  
+export default Footer;

48b4207d4120d35aaf4de82b0ea69fad7bad60c6
Elijah Littell <elittell214@gmail.com>
2021-01-15 03:09:25 -0500

    about

--- lookatmefile/src/components/Home.js
+++ lookatmefile/src/components/Home.js
@@ -4,6 +4,7 @@ function Home() {
     return (
       <div>
           <h1>Welcome to Lookatme.me :)</h1>
+          <p>About? We are a community hoping to inspire others and motivate and encourage what they are doing! Positivity is everything in here and we are welcoming everyone!</p>
       </div>
     );
 }

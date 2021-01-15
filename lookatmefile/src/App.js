68209a369df09d2e72ae521734ac7c4c1dd681ec
Elijah Littell <elittell214@gmail.com>
2021-01-15 01:54:37 -0500

    Added Router, Switch, and Link! Getting the home page ready

--- lookatmefile/src/App.js
+++ lookatmefile/src/App.js
@@ -1,10 +1,29 @@
 import LogIn from './components/LogIn';
+import { Link, Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h1>HI</h1>
-      <LogIn />
+      <Router>
+
+        <div className="header">
+          <input type="search" />
+          <Link to="/">Home</Link>
+          <Link to="/login">Login</Link>
+        </div>
+    
+        <hr />
+    
+        <Switch>
+          <Route exact path="/">
+            <h1>This is going to be the home page</h1>
+          </Route>
+          <Route path="/login">
+            <LogIn />
+          </Route>
+        </Switch>
+
+      </Router>  
     </div>
   );
 }
